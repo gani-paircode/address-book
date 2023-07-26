5501495b147df210f607a5159b08829ef455cb6f
Ghanshyam <gk.paircode@gmail.com>
2023-07-27 01:18:21 +0530

    wip

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,8 +5,8 @@ const SHA256 = require("crypto-js/sha256");
 const _ = require("lodash");
 const { LOGIN_FILE_PATH, COOKIES_NAME } = require('../constants/general');
 const { readDataFromFile, writeDataToFile } = require('../model/files');
-const authenticateReq = require('../index').authenticateReq;
-
+const { authenticateReq } = require('../index');
+console.log('authenticateReq --> ', authenticateReq);
 const getHashedPassword = (plainPassword) => SHA256(plainPassword).toString();
 const generateLoginToken = (phoneNumber) => {
   const p1 = `${Math.random() * phoneNumber}`.replace(".", "");
@@ -15,44 +15,44 @@ const generateLoginToken = (phoneNumber) => {
   return _.shuffle(batakaa).join("-");
 };
 
-router.post('/v1/update-password',  async (req, res) => {
-  const { currentPassword, password, repeatPassword } = req.body;
-  console.log('credentials in login/v1 ', { currentPassword, password, repeatPassword });
-  try {
-    const currentHashedPassword = getHashedPassword(currentPassword);
-    let users = await readDataFromFile(LOGIN_FILE_PATH);
-    const user = users.find((u) => 
+router.post('/v1/update-password', authenticateReqCopy, async (req, res) => {
+    const { currentPassword, password, repeatPassword } = req.body;
+    console.log('credentials in login/v1 ', { currentPassword, password, repeatPassword });
+    try {
+      const currentHashedPassword = getHashedPassword(currentPassword);
+      let users = await readDataFromFile(LOGIN_FILE_PATH);
+      const user = users.find((u) =>
         req.headers[COOKIES_NAME.PHONE] === u.phoneNumber
-    );
-    if (!user) {
-      res.status(400).send({ message: 'User not found' });
-      return;
-    }
-    if (user.isActive === false) {
-      res.status(403).send({ message: 'User is deactivated. Please contact admin.' });
-      return;
-    }
- 
-    if(user.hashedPassword !== currentHashedPassword){
-      res.status(403).send({ message: 'Current password is invalid' });
-      return;
-    }
+      );
+      if (!user) {
+        res.status(400).send({ message: 'User not found' });
+        return;
+      }
+      if (user.isActive === false) {
+        res.status(403).send({ message: 'User is deactivated. Please contact admin.' });
+        return;
+      }
 
-    if(password !== repeatPassword) {
-      res.status(403).send({ message: 'New password and repeat password are not same' });
+      if (user.hashedPassword !== currentHashedPassword) {
+        res.status(403).send({ message: 'Current password is invalid' });
+        return;
+      }
+
+      if (password !== repeatPassword) {
+        res.status(403).send({ message: 'New password and repeat password are not same' });
+        return;
+      }
+
+      user.hashedPassword = getHashedPassword(password);
+      await writeDataToFile(LOGIN_FILE_PATH, users);
+      res.json(true);
+      return;
+    } catch (error) {
+      console.log('Error -> ', error);
+      res.status(500).send({ message: 'Internal Server Error' });
       return;
     }
-    
-    user.hashedPassword = getHashedPassword(password);
-    await writeDataToFile(LOGIN_FILE_PATH, users);
-    res.json(true);
-    return;
-} catch (error) {
-  console.log('Error -> ', error);
-    res.status(500).send({ message: 'Internal Server Error' });
-    return;
-}
-});
+  });
 
 router.post('/v1/login', async (req, res) => {
   const { phoneNumber, password } = req.body;
@@ -68,7 +68,7 @@ router.post('/v1/login', async (req, res) => {
       res.status(400).send({ message: 'User is deactivated. Please contact admin.' });
       return;
     }
- 
+
     const hashedPassword = getHashedPassword(password);
     if (user.hashedPassword !== hashedPassword) {
       res.status(400).send({ message: 'Invalid password' });
@@ -82,12 +82,31 @@ router.post('/v1/login', async (req, res) => {
     console.log('------------> ', { loginToken, phoneNumber });
     res.json({ loginToken, phoneNumber });
     return;
-} catch (error) {
-  console.log('Error -> ', error);
+  } catch (error) {
+    console.log('Error -> ', error);
     res.status(500).send({ message: 'Internal Server Error' });
     return;
-}
+  }
 });
 
+
+async function authenticateReqCopy(req, res, next) {
+  const { originalUrl } = req;
+  /* why we need to rely on headers ? bcz be and fe are on different domains */
+  const ckToken = req.cookies[COOKIES_NAME.TOKEN] || req.headers[COOKIES_NAME.TOKEN];
+  const ckuserPn = req.cookies[COOKIES_NAME.PHONE] || req.headers[COOKIES_NAME.PHONE];
+  try {
+      const loginUsers = await readDataFromFile(LOGIN_FILE_PATH);
+      const index = loginUsers.findIndex(u => u.phoneNumber === ckuserPn && u.token === ckToken);
+      if (index === -1) {
+          res.status(403).send({ message: 'Unauthorized request' });
+          return;
+      }
+      next();
+  } catch (error) {
+      console.log('Error in authenticateReq ', error);
+      res.status(500).send({ message: 'Internal Server Error' });
+  }
+}
 
 module.exports = router;
